9ec9102b641b3d7903e030f48f5d054093fb30d9
parkwoojoo <donghun54@naver.com>
2024-05-10 17:18:45 +0900

    second

--- LiveCommerce/src/main/java/org/zerock/controller/LoginController.java
+++ LiveCommerce/src/main/java/org/zerock/controller/LoginController.java
@@ -1,48 +1,68 @@
 package org.zerock.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.zerock.domain.UserVO;
+import org.zerock.service.UserService;
 
 import lombok.extern.log4j.Log4j;
 
 @Controller
 @Log4j
 public class LoginController {
-	
-	@GetMapping("/accessError")
-	public void accessDenied(Authentication auth, Model model) {
-		
-		log.info("-------------------------------------");
-		log.info(auth);
-		
-		model.addAttribute("msg", "접근 권한이 없습니다.");
-	}
-	
-	@GetMapping("/customLogin")
-	public void loginInput(String error, String logout, Model model) {
-		
-		log.info("error : " + error);
-		log.info("logout : " + logout);
-		
-		if(error != null) {
-			model.addAttribute("error", "아이디 or 비번이 틀립니다.");
-		}
+   
+   @Autowired
+    private UserService userService;
+   
+   @GetMapping("/accessError")
+   public void accessDenied(Authentication auth, Model model) {
+      
+      log.info("-------------------------------------");
+      log.info(auth);
+      
+      model.addAttribute("msg", "접근 권한이 없습니다.");
+   }
+   
+   @GetMapping("/customLogin")
+   public void loginInput(String error, String logout, Model model) {
+      
+      log.info("error : " + error);
+      log.info("logout : " + logout);
+      
+      if(error != null) {
+         model.addAttribute("error", "아이디 or 비번이 틀립니다.");
+      }
 
-		
-		if(logout != null) {
-			model.addAttribute("logout", "로그아웃!!!!");
-		}
-	}
-	
+      
+      if(logout != null) {
+         model.addAttribute("logout", "로그아웃!!!!");
+      }
+      
+   }
+   
      @GetMapping("/customLogout")
-	public void logoutGET() {
-		log.info("custom logout.........");
-	}
-    
-    }
+   public void logoutGET() {
+      log.info("custom logout.........");
+   }
+     
+     
+     @GetMapping("/user/register")
+     public String registerForm(Model model) {
+         model.addAttribute("user", new UserVO());
+         return "/user/registerForm"; // 회원가입 폼으로 이동
+     }
 
+     @PostMapping("/user/register")
+     public String registerSubmit(@ModelAttribute UserVO user) {
+         userService.registerUser(user);
+         return "redirect:/user/login"; // 회원가입 성공 시 로그인 페이지로 이동
+     }
+    
+}
 
--- LiveCommerce/src/main/webapp/WEB-INF/views/user/registerForm.jsp
+++ LiveCommerce/src/main/webapp/WEB-INF/views/user/registerForm.jsp
@@ -1,12 +1,34 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-<meta charset="UTF-8">
-<title>Insert title here</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Registration</title>
 </head>
 <body>
-${user}
+    <h2>User Registration</h2>
+    <form action="/register" method="post">
+        <div>
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username" required>
+        </div>
+        <div>
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" required>
+        </div>
+        <div>
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" required>
+        </div>
+        <div>
+            <label for="shippingAddress">Shipping Address:</label>
+            <input type="text" id="shippingAddress" name="shippingAddress">
+        </div>
+        <div>
+            <label for="shippingPostalCode">Shipping Postal Code:</label>
+            <input type="text" id="shippingPostalCode" name="shippingPostalCode">
+        </div>
+        <button type="submit">Register</button>
+    </form>
 </body>
 </html>
