6bc8021b44f163975dde88e455f4741c807fc52f
parkwoojoo <donghun54@naver.com>
2024-05-14 17:57:26 +0900

    second

--- LiveCommerce/src/main/java/org/zerock/controller/CartController.java
+++ LiveCommerce/src/main/java/org/zerock/controller/CartController.java
@@ -9,10 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.zerock.domain.CartVO;
-import org.zerock.domain.ProductVO;
+import org.zerock.domain.CartProductVO;
 import org.zerock.service.CartService;
-import org.zerock.service.ProductService;
 import org.zerock.service.UserService;
 
 import lombok.extern.log4j.Log4j;
@@ -22,48 +20,29 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 public class CartController {
 
-    private final CartService cartService;
-    private final ProductService productService;
+	private final CartService cartService;
     private final UserService userService;
 
     @Autowired
-    public CartController(CartService cartService, ProductService productService, UserService userService) {
+    public CartController(CartService cartService, UserService userService) {
         this.cartService = cartService;
-        this.productService = productService;
         this.userService = userService;
     }
 
-    // 메인 페이지
     @GetMapping("/list")
-    public String home(Model model) {
+    public String viewCart(Model model) {
         // 현재 로그인한 사용자의 정보 가져오기
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String username = authentication.getName();
-        log.info(username);
         
-        // 사용자 정보를 이용하여 userID 가져오기
+        // 사용자의 ID로 장바구니 정보 가져오기
         int userID = userService.getUserIDByUsername(username);
-        log.info(userID);
-
-        // 사용자의 장바구니 정보 가져오기
-        List<CartVO> carts = cartService.getCartByUserID(userID);
-        model.addAttribute("carts", carts);
-        log.info(carts);
-
-        // 상품 정보를 가져와서 모델에 추가
-        List<ProductVO> products = productService.getAllProducts();
-        model.addAttribute("products", products);
-
-        // 현재 로그인한 사용자 정보 가져와서 모델에 추가 (예시)
-        // 사용자 이름이나 다른 정보를 화면에 표시할 수 있음
-        model.addAttribute("username", username);
-
-        return "/live/cart";
-    }
-    @GetMapping("/details")
-    public String getCartDetails(Model model, int userID) {
-        List<CartVO> cartDetails = cartService.getCartDetails(userID);
+        List<CartProductVO> cartDetails = cartService.getCartDetails(userID);
+        
+        // 장바구니 정보와 사용자 정보를 모델에 추가
         model.addAttribute("cartDetails", cartDetails);
-        return "cartDetails"; // 장바구니 상세 정보를 표시하는 뷰의 이름
+        model.addAttribute("userID", userID);
+        
+        return "/live/cart";
     }
 }
--- LiveCommerce/src/main/webapp/WEB-INF/views/live/cart.jsp
+++ LiveCommerce/src/main/webapp/WEB-INF/views/live/cart.jsp
@@ -1,37 +1,42 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
-<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="UTF-8">
     <title>장바구니</title>
 </head>
 <body>
     <h2>장바구니</h2>
-    ${products}
     
-    <table>
+    <table border="1">
         <thead>
             <tr>
                 <th>상품명</th>
                 <th>가격</th>
                 <th>수량</th>
+                <th>총 가격</th>
                 <!-- 기타 필요한 컬럼들 -->
             </tr>
         </thead>
         <tbody>
-            <!-- 장바구니 목록을 반복해서 표시 -->
-            <c:forEach items="${carts}" var="cart">
+            <!-- 장바구니 정보를 반복해서 표시 -->
+            <c:forEach items="${cartDetails}" var="cartItem">
                 <tr>
-                 
-                    <td>${cart.productID}</td>
-                    <td>${cart.userID}</td>
-                    
-                   
-                    
+                    <!-- 상품명 -->
+                    <td>${cartItem.productName}</td>
+                    <!-- 가격 -->
+                    <td>${cartItem.price}</td>
+                    <!-- 수량 -->
+                    <td>${cartItem.quantity}</td>
+                    <!-- 총 가격 = 가격 * 수량 -->
+                    <td>${cartItem.price * cartItem.quantity}</td>
+                    <!-- 기타 필요한 컬럼들 -->
                 </tr>
             </c:forEach>
         </tbody>
     </table>
+
+    <!-- 로그인한 사용자의 ID -->
+    <p>사용자 ID: ${userID}</p>
 </body>
 </html>
